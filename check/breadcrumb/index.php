4fe819e9d7ea457fd1037192964d3dc09294cfe8
Daniil-Padiryakkov <daniilpadiryakov@gmail.com>
2021-03-30 14:31:43 +0300

    lesson1

---
+++ check/breadcrumb/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once ($_SERVER["DOCUMENT_ROOT"] . '/bitrix/header.php');
+
+/** @global CMain $APPLICATION */
+$APPLICATION->SetTitle('Breadcrumb');
+
+echo 'Breadcrumb Breadcrumb';
+
+require_once ($_SERVER["DOCUMENT_ROOT"] . '/bitrix/footer.php');
---
+++ test/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+echo "111111";
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
